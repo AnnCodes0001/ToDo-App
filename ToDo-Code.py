33c85bcfe2d70e24a388847eb7ff6e966a84d7d2
Anncodes001 <annalexx2001@gmail.com>
2023-09-17 13:28:57 -0500

    more corrections

--- ToDo-Code.py
+++ ToDo-Code.py
@@ -1,9 +1,10 @@
+Due_Date = input('Enter Date: ')
 Task_Description = input('Enter Task:')
 
 Options = ('High', 'Low')
 
 Priority = input(f'Pick from one of these{Options}: ')
-Due_Date = input('Enter Date')
+
 
 def Task_Entry():
     if Priority in Options:
@@ -14,13 +15,13 @@ def Task_Entry():
 #i don't know why return isn't working i'll be back to look at it though
 #return Priority
 
-class Entry:
-    def Data_Entry(Self,Task_Description,Priority,Due_Date):
-        self.Task_Description = Task_Description
-        self.Priority = Priority
-        self.Due_Date = Due_Date
+#class Entry:
+    #def Data_Entry(self,Task_Description,Priority,Due_Date):
+     #   self.Task_Description = Task_Description
+      #  self.Priority = Priority
+       # self.Due_Date = Due_Date
 
-        print(f"You've Entered: Task_Description, Priority, Due_Date")
+        print(f"You've Entered: {self.Task_Description}, {self.Priority}, {self.Due_Date}")
         
 
 
